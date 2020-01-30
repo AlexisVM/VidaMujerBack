df7cd7e725a485023bf824bfe6212aab49df3dca
root <root@LAPTOP-GPTBOQ5R.localdomain>
2020-01-30 14:58:39 -0600

    modified models with cache

--- api/models.py
+++ api/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth import authenticate, get_user_model
-
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 User = get_user_model()
 
 class Tip(models.Model):
 	titulo = models.CharField(max_length=100,null=True, blank=True)
 	desc = models.TextField()
-	imagen = models.FileField(null=True, blank=True)
+	imagen = models.ImageField(upload_to='tips')
+	photo_thumbnail = ImageSpecField(source='imagen',
+									  processors=[ResizeToFill(250, 250)],
+									  format='JPEG',
+									  options={'quality': 60})
 	class Meta:
 		verbose_name_plural = "Tips"
 		verbose_name = "Tip"
@@ -18,12 +23,20 @@ class Tip(models.Model):
 class Concepto(models.Model):
 	titulo = models.CharField(max_length=100)
 	desc = models.TextField()
-	imagen = models.FileField()
+	imagen = models.ImageField(upload_to='conceptos')
+	photo_thumbnail = ImageSpecField(source='imagen',
+									  processors=[ResizeToFill(250, 250)],
+									  format='JPEG',
+									  options={'quality': 60})
 
 class Paquete(models.Model):
 	titulo = models.CharField(max_length=100)
 	desc = models.TextField()
-	imagen = models.FileField()
+	imagen = models.ImageField(upload_to='paquetes')
+	photo_thumbnail = ImageSpecField(source='imagen',
+									  processors=[ResizeToFill(250, 250)],
+									  format='JPEG',
+									  options={'quality': 60})
 	consulta = models.BooleanField()
 	costo = models.PositiveSmallIntegerField()
 	usuarios = models.ManyToManyField(User,through='Compra')
@@ -35,3 +48,12 @@ class Compra(models.Model):
 	paquete = models.ForeignKey(Paquete, on_delete=models.SET_NULL, null=True)
 	usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 	fecha_de_pago = models.DateTimeField()
+
+class Medicamento(models.Model):
+	nombre = models.CharField(max_length=100)
+	imagen = models.ImageField(upload_to='paquetes')
+	photo_thumbnail = ImageSpecField(source='imagen',
+									  processors=[ResizeToFill(250, 250)],
+									  format='JPEG',
+									  options={'quality': 60})
+	costo = models.PositiveSmallIntegerField()
